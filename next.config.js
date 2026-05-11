06476a9d6eba401b3edf28543e873a7ab950d4d4
Ali Nawaf <ali.akramnd@gmail.com>
2026-05-11 13:39:12 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  async rewrites() {
+    return [
+      {
+        source: '/alijr',
+        destination: 'https://personal-qm2fsvczr-ali-nawafs-projects.vercel.app/alijr',
+      },
+      {
+        source: '/alijr/:path*',
+        destination: 'https://personal-qm2fsvczr-ali-nawafs-projects.vercel.app/alijr/:path*',
+      },
+    ]
+  },
   images: {
     domains: ['images.unsplash.com', 'via.placeholder.com'],
     unoptimized: true
